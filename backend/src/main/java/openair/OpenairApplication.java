504f3e68f0c77b54d94e1bb20a65668dbae12ff2
antonic901 <antonicnikola6@gmail.com>
2021-12-07 11:27:09 +0100

    added empty spring boot

---
+++ backend/src/main/java/openair/OpenairApplication.java
@@ -0,0 +1,13 @@
+package openair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenairApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/openair/OpenairApplicationTests.java
@@ -0,0 +1,13 @@
+package openair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
