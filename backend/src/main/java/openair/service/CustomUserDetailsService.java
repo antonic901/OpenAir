83725526c8f9bf587dd41d1bc41339c368fdab97
Anđela Paunović <andjelapaunovic99@gmail.com>
2021-12-08 13:34:20 +0100

    Added spring security configuration

---
+++ backend/src/main/java/openair/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package openair.service;
+
+import openair.model.User;
+import openair.repository.UserRepository;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("No user found with username '%s'.", username));
+        } else {
+            return user;
+        }
+    }
+
+
+}
---
+++ backend/src/main/java/openair/service/UserService.java
@@ -0,0 +1,31 @@
+package openair.service;
+
+import openair.model.User;
+import openair.repository.UserRepository;
+import openair.service.interfaces.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+//    @Autowired
+//    public UserService(UserRepository userRepository){
+//        this.userRepository = userRepository;
+//    }
+
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+}
