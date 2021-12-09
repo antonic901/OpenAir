c24994e3b4e15e6b4aec89420c8752af04d53084
Anđela Paunović <andjelapaunovic99@gmail.com>
2021-12-09 11:14:45 +0100

    Feature add project done

---
+++ backend/src/main/java/openair/service/ProjectService.java
@@ -0,0 +1,36 @@
+package openair.service;
+
+import liquibase.pro.packaged.P;
+import openair.dto.ProjectDTO;
+import openair.model.Admin;
+import openair.model.Project;
+import openair.repository.ProjectRepository;
+import openair.service.interfaces.IProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService implements IProjectService {
+
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository){
+        this.projectRepository = projectRepository;
+    }
+
+    @Override
+    public Project findProjectByName(String name) {
+        return projectRepository.findByName(name);
+    }
+
+    @Override
+    public Project addProject(ProjectDTO projectDTO, Admin admin){
+        Project project = new Project();
+
+        project.setAdmin(admin);
+        project.setName(projectDTO.getName());
+
+        return projectRepository.save(project);
+    }
+}
