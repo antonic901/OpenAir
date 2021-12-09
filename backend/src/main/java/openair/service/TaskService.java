5f9599cc154351260de793d87d595ee1b673cd30
StrahinjaBb <straleb123@gmail.com>
2021-12-09 15:19:41 +0100

    feature: add-task-to-project-v1

--- backend/src/main/java/openair/service/TaskService.java
+++ backend/src/main/java/openair/service/TaskService.java
@@ -3,13 +3,16 @@ package openair.service;
 import openair.dto.TaskDTO;
 import openair.model.Project;
 import openair.model.Task;
+import openair.repository.ProjectRepository;
 import openair.repository.TaskRepository;
 import openair.service.interfaces.ITaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class TaskService implements ITaskService {
+    private ProjectRepository projectRepository;
     private TaskRepository taskRepository;
 
     @Autowired
@@ -28,6 +31,20 @@ public class TaskService implements ITaskService {
         Task task = new Task();
         task.setProject(project);
         task.setName(taskDTO.getName());
+
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public Task addTaskToProject(Long taskId, Long projectId) {
+        Project project = projectRepository.findById(projectId).get();
+        Task task = taskRepository.findById(taskId).get();
+
+        List<Task> taskList = project.getTasks();
+        if(!taskList.contains(task))
+            taskList.add(task);
+
+        project.setTasks(taskList);
 
         return taskRepository.save(task);
     }
--- backend/src/main/java/openair/service/interfaces/ITaskService.java
+++ backend/src/main/java/openair/service/interfaces/ITaskService.java
@@ -7,4 +7,5 @@ import openair.model.Task;
 public interface ITaskService {
     Task findTaskByName(String name);
     Task addTask(TaskDTO taskDTO, Project project);
+    Task addTaskToProject(Long taskId, Long projectId);
 }
