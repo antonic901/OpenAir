719abb781f05364d0dffec085327185395c987a3
Anđela Paunović <andjelapaunovic99@gmail.com>
2021-12-13 17:03:09 +0100

    My changes

--- backend/src/main/java/openair/service/EmployeeService.java
+++ backend/src/main/java/openair/service/EmployeeService.java
@@ -135,7 +135,10 @@ public class EmployeeService implements IEmployeeService {
         List<LocalDate> filledDates = new ArrayList<>();
 
         //treba proci kroz listu timeSheetDays i povaditi datume u filledDates
+        for (TimeSheetDay ts: timeSheetDays
+             ) {
 
+        }
 
         return filledDates;
     }
