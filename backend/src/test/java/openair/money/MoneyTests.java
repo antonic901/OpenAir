12f728629328cf4f6211c5e2e5ee949448698f03
antonic901 <antonicnikola6@gmail.com>
2021-12-14 19:16:19 +0100

    added tests for MoneyService methods

---
+++ backend/src/test/java/openair/money/MoneyTests.java
@@ -0,0 +1,45 @@
+package openair.money;
+
+
+import openair.repository.MoneyRepository;
+import openair.service.MoneyService;
+import org.assertj.core.data.Percentage;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class MoneyTests {
+
+    @Mock
+    private MoneyRepository moneyRepository;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Spy
+    @InjectMocks
+    private MoneyService moneyService;
+
+    @Test
+    public void testConvert() {
+        double eurToRsd = moneyService.convert("2020-12-11", "EUR", "RSD", 20);
+        double rsdToEur = moneyService.convert("2020-12-11", "RSD", "EUR", 12000);
+
+        assertThat(eurToRsd).isCloseTo(2400, Percentage.withPercentage(5));
+        assertThat(rsdToEur).isCloseTo(100, Percentage.withPercentage(3));
+    }
+
+}
