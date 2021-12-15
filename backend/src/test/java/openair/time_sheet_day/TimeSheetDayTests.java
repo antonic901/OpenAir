15a77cb344d8dcaae169e46b4e6ee361f6aad53b
StrahinjaBb <straleb123@gmail.com>
2021-12-15 15:46:09 +0100

    TimeSheetDayTest bug fixed

--- backend/src/test/java/openair/time_sheet_day/TimeSheetDayTests.java
+++ backend/src/test/java/openair/time_sheet_day/TimeSheetDayTests.java
@@ -3,10 +3,12 @@ package openair.time_sheet_day;
 import openair.absence.TestData;
 import openair.dto.TimeSheetDayDTO;
 import openair.model.Employee;
+import openair.model.Task;
 import openair.model.TimeSheetDay;
 import openair.repository.EmployeeRepository;
 import openair.repository.TaskRepository;
 import openair.repository.TimeSheetDayRepository;
+import openair.service.TaskService;
 import openair.service.TimeSheetDayService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -29,20 +31,23 @@ public class TimeSheetDayTests {
     @Mock
     private TaskRepository taskRepository;
 
+    @Mock
+    private TaskService taskService;
+
     @Spy
     @InjectMocks
     private TimeSheetDayService timeSheetDayService;
 
-//    //  method args: timeSheetDayDTO, Employee (object, not Long id)
-//    @Test
-//    public void testAddTimeSheetDay() {
-//        //  given
-//        Employee employee = TestData.createEmployee();
-//        TimeSheetDayDTO timeSheetDayDTO = TimeSheetDayTestData.createTimeSheetDayDTO();
-//        //  when
-//        when(timeSheetDayRepository.save(any(TimeSheetDay.class))).thenReturn(new TimeSheetDay());
-//
-//        //  then
-//        assertThat(timeSheetDayService.addTimeSheetDay(timeSheetDayDTO, employee)).isNotNull();
-//    }
+    //  method args: timeSheetDayDTO, Employee (object, not Long id)
+    @Test
+    public void testAddTimeSheetDay() {
+        //  given
+        Employee employee = TestData.createEmployee();
+        TimeSheetDayDTO timeSheetDayDTO = TimeSheetDayTestData.createTimeSheetDayDTO();
+        //  when
+        when(timeSheetDayRepository.save(any(TimeSheetDay.class))).thenReturn(new TimeSheetDay());
+
+        //  then
+        assertThat(timeSheetDayService.addTimeSheetDay(timeSheetDayDTO, employee)).isNotNull();
+    }
 }
