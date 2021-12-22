e71447ff0176f7bdff43ad09b6a2b2865da17a9a
Anđela Paunović <andjelapaunovic99@gmail.com>
2021-12-22 10:03:56 +0100

    Admin creates timesheet for employee

--- backend/src/main/java/openair/model/Admin.java
+++ backend/src/main/java/openair/model/Admin.java
@@ -1,14 +1,9 @@
 package openair.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @DiscriminatorValue("Admin")
